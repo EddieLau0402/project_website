3ce0ce9098321d2ca36f7caf6a543cddb6e9d509
Eddie Lau <eddielau@EddiedeMacBook-Air.local>
2014-12-31 02:51:06 +0800

    build up a website basic framework.

---
+++ index.php
@@ -0,0 +1,61 @@
+<?php 
+/**
+ * Index page.
+ *
+ * @author Eddie
+ */
+?>
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Website</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta http-equiv="imagetoolbar" content="no" />
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<meta name="author" content="Eddie" />
+<meta name="generator" content="Eddie" />
+
+<!-- stylesheet -->
+<link rel="stylesheet" href="./asset/css/bootstrap.min.css">
+<link rel="stylesheet" href="./asset/css/bootstrap-theme.min.css">
+<link rel="stylesheet" href="./asset/css/font-awesome.min.css">
+<link rel="stylesheet" href="./asset/css/font-awesome-ie7.min.css">
+<link rel="stylesheet" href="./asset/css/init.css">
+</head>
+
+<body>
+  <script src="./asset/js/jquery-1.11.1.min.js"></script>
+
+  <!-- Header -->
+  <header>
+  	<div class="container">
+  	  <div id="logo" class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
+  	    <a href="#"></a>
+  	  </div>
+  	</div>
+  </header>
+
+  <!-- Container -->
+  <div></div>
+
+  <!-- Footer -->
+  <footer></footer>
+
+  <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+  <!--[if lt IE 9]><script src="./asset/js/ie8-responsive-file-warning.js"></script><![endif]-->
+  <script src="./asset/js/ie-emulation-modes-warning.js"></script>
+  <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+  <script src="./asset/js/ie10-viewport-bug-workaround.js"></script>
+  <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+  <script src="./asset/js/bootstrap.min.js"></script>
+  <script src="./asset/js/bootstrap-hover-dropdown.min.js"></script>
+
+</body>
+</html>
