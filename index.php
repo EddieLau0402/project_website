4938650587d6180da2309a67cdbcd8c599f5e4cc
Eddie <277676978@qq.com>
2014-12-31 18:04:25 +0800

    modify 'index.php'

--- index.php
+++ index.php
@@ -32,14 +32,71 @@
   <!-- Header -->
   <header>
   	<div class="container">
+         <!-- Logo -->
   	  <div id="logo" class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
   	    <a href="#"></a>
   	  </div>
+         <!-- Nav-bar -->
+         <nav id="nav" class="navbar navbar-default col-lg-10 col-md-10 col-sm-10 col-xs-12" role="navigation">
+           <div class="container-fluid">
+             <!-- Small icon -->
+             <div class="navbar-header">
+               <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-nav-menu">
+                 <span class="sr-only">Toggle navigation</span>
+                 <span class="icon-bar"></span>
+                 <span class="icon-bar"></span>
+                 <span class="icon-bar"></span>
+               </button>
+             </div>
+             <!-- Nav items -->
+             <div class="collapse navbar-collapse" id="main-nav-menu">
+               <ul class="nav navbar-nav">
+                 <li>
+                   <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-delay="100">Categories <span class="caret"></span></a>
+                   <ul class="dropdown-menu" role="menu">
+                     <li><a href="#">cate-1</a></li>
+                     <li><a href="#">cate-2</a></li>
+                     <li><a href="#">cate-3</a></li>
+                   </ul>
+                 </li>
+                 <li><a>About Us</a></li>
+                 <li><a>Contact Us</a></li>
+               </ul>
+             </div>
+           </div>
+         </nav>
   	</div>
   </header>
 
   <!-- Container -->
-  <div></div>
+  <div>
+    <!-- Slide banner -->
+    <div id="top-banner" class="carousel slide" data-ride="carousel">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li data-target="#top-banner" data-slide-to="1" class="active"><span></span></li>
+        <li data-target="#top-banner" data-slide-to="2" class="active"><span></span></li>
+        <li data-target="#top-banner" data-slide-to="3" class="active"><span></span></li>
+      </ol>
+      <!-- Banner-images -->
+      <div class="carousel-inner" role="listbox">
+        <div class="item active"><img src="" /></div>
+        <div class="item"><img src="" /></div>
+        <div class="item"><img src="" /></div>
+      </div>
+      <!-- Controls -->
+      <a class="left carousel-control glyphicon glyphicon-chevron-left" href="#top-banner" role="button" data-slide="prev">
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="right carousel-control glyphicon glyphicon-chevron-right" href="#top-banner" role="button" data-slide="next">
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+
+    <!-- Content -->
+    <div></div>
+
+  </div>
 
   <!-- Footer -->
   <footer></footer>
